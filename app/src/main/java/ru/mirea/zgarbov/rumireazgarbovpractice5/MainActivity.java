d7fce243a6c77760cc9e7f5dc8e5875321ddd02d
Ferzerino <ferzchannel@gmail.com>
2021-05-30 15:35:04 -0700

    Practice6

---
+++ app/src/main/java/ru/mirea/zgarbov/rumireazgarbovpractice5/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.zgarbov.rumireazgarbovpractice5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
